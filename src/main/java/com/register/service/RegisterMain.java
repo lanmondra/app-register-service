5666cd802f59c18aee60afc26b377da5304cc265
Miguel <miguelmondragon@iMac-de-Miguel.local>
2022-11-12 15:58:07 +0100

    cambio descripcion del main

--- src/main/java/com/register/service/RegisterMain.java
+++ src/main/java/com/register/service/RegisterMain.java
@@ -7,14 +7,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 
-@OpenAPIDefinition(info = @Info(title = "proyecto en blanco", version = "2.0", description = "Information"))
+@OpenAPIDefinition(info = @Info(title = "Registro de usurios ", version = "2.0", description = "Recopila toda la info de los usuarios "))
 @SpringBootApplication
 public class RegisterMain {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterMain.class, args);
 
-		// test develop
+
 	}
 
 }
